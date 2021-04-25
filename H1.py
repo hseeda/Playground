55e64c900f65ca423812dfb05975abf8545efcfe
HMA <hseeda1@icloud.com>
2021-04-25 22:16:46 +0200

    h1

---
+++ H1.py
@@ -0,0 +1,27 @@
+import os
+from os import listdir
+from os.path import isfile, join
+from pprint import pprint
+
+
+# print (os.getcwd())
+# print (os.getlogin())
+
+# env = os.environ.__dict__["_data"]
+# pprint.pprint(env)
+
+mypath = os.getcwd()
+# onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+
+for dirpath, dirs, files in os.walk(mypath):
+  for file in files:
+    file_name = os.path.join(dirpath, file)
+    print(file_name)
+    # if file_name.endswith('.py'):
+    #     print(file_name)
+
+for dirpath, dirs, files in os.walk(mypath):
+  for file in files:
+    file_name = os.path.join(dirpath, file)
+    print(file_name)
