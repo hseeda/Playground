93f0087f0ef9ddded090219e994bc2d214811fc9
HMA <hseeda1@icloud.com>
2021-04-25 22:29:57 +0200

    h3

--- H1.py
+++ H1.py
@@ -14,16 +14,25 @@ mypath = os.getcwd()
 # onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 
-for dirpath, dirs, files in os.walk(mypath):
-  for file in files:
-    file_name = os.path.join(dirpath, file)
-    print(file_name)
-    # if file_name.endswith('.py'):
-    #     print(file_name)
+# for dirpath, dirs, files in os.walk(mypath):
+#   for file in files:
+#     file_name = os.path.join(dirpath, file)
+#     print(file_name)
+#     # if file_name.endswith('.py'):
+#     #     print(file_name)
 
-for dirpath, dirs, files in os.walk(mypath):
-  for file in files:
-    file_name = os.path.join(dirpath, file)
-    print(file_name)
+# for dirpath, dirs, files in os.walk(mypath):
+#   for file in files:
+#     file_name = os.path.join(dirpath, file)
+#     print(file_name)
 
-    
+
+
+w = os.walk(mypath)
+d = w.__dir__
+
+print(d)
+
+# print(d)
+# for dirpath, dirs, files in os.walk(mypath):
+#     print(dirs)
